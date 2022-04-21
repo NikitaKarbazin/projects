f15cb5bfdc093481754936dfcba465e0b93354d6
Nikita Karabazin <karbazinnikita@gmail.com>
2022-04-21 10:40:24 +0300

    changes

--- JavaScript30/JS_CSS_Clock/script.js
+++ JavaScript30/JS_CSS_Clock/script.js
@@ -1,24 +1,21 @@
-const secondHand = document.querySelectorAll('.second-hand');
-const minutesHand = document.querySelectorAll('.min-hand');
-const hourHand = document.querySelectorAll('.hour-hand');
+const secondHand = document.querySelector('.second-hand');
+const minutesHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
   const now = new Date();
 
   const second = now.getSeconds();
   const secDeg = (second / 60) * 360 + 90;
-  secondHand[0].style.transform =  `rotate(${secDeg}deg)`;
-  secondHand[1].style.transform =  `rotate(${secDeg}deg)`;
+  secondHand.style.transform =  `rotate(${secDeg}deg)`;
 
   const minute = now.getMinutes();
   const minDeg = (minute / 60) * 360 + 90;
-  minutesHand[0].style.transform = `rotate(${minDeg}deg)`;
-  minutesHand[1].style.transform = `rotate(${minDeg}deg)`;
+  minutesHand.style.transform = `rotate(${minDeg}deg)`;
 
   const hour = now.getHours();
   const hourDeg = (hour / 12) * 360 + 90;
-  hourHand[0].style.transform = `rotate(${hourDeg}deg)`;
-  hourHand[1].style.transform = `rotate(${hourDeg}deg)`;
+  hourHand.style.transform = `rotate(${hourDeg}deg)`;
 
 }
 
